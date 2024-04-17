e8827bc5276dd408d82988c73f66105fd9ef71b9
mizchi <miz404@gmail.com>
2024-04-17 21:29:07 +0900

    simple binary encoder

--- js_ffi/main/encoder.ts
+++ js_ffi/main/encoder.ts
@@ -10,46 +10,47 @@ enum DataType {
 
 type BinaryFormat = [
   dataType: DataType,
-  len: number,
+  // ...length: number[], 
   ...number[]
 ];
 
 type ArrayBinaryFormat = [
   dataType: DataType.Array,
   len: number,
-  itemCount: number,
   ...number[]
 ];
 
 const encoder = new TextEncoder();
 const decoder = new TextDecoder();
 
-function encodeInt(value: number): BinaryFormat {
+function intToBytes(value: number): Uint8Array {
   const buffer = new ArrayBuffer(4);
   const dataView = new DataView(buffer);
   dataView.setInt32(0, value);
-  return [DataType.Int, dataView.byteLength, ...new Uint8Array(buffer)]
+  return new Uint8Array(buffer);
+}
+
+function encodeLength(len: number): [len: number, ...number[]] {
+  const buf = intToBytes(len);
+  return [buf.byteLength, ...buf];
+}
+
+function encodeInt(value: number): BinaryFormat {
+  const buf = intToBytes(value);
+  return [DataType.Int, ...encodeLength(buf.byteLength), ...buf]
 }
 
 function encodeFloat(value: number): BinaryFormat {
   const buffer = new ArrayBuffer(8);
   const dataView = new DataView(buffer);
   dataView.setFloat64(0, value);
-  return [DataType.Float, dataView.byteLength, ...new Uint8Array(buffer)];
+  return [DataType.Float, ...encodeLength(dataView.byteLength), ...new Uint8Array(buffer)];
 }
 
 function encodeString(value: string): BinaryFormat {
   const encodedStr = encoder.encode(value);
-  return [DataType.String, encodedStr.byteLength, ...encodedStr];
+  return [DataType.String, ...encodeLength(encodedStr.byteLength), ...encodedStr];
 }
-
-
-// function encodeArray(arr: Array<string | number>): Uint8Array {
-//   const encodedArr = encodeData(arr);
-//   const len = encodedArr.byteLength;
-//   const bytes = [DataType.Array, arr.length, len, ...encodedArr];
-//   return 
-// }
 
 type Item = string | number | Array<Item>;
 
@@ -61,7 +62,7 @@ export function encodeItem(item: Item): number[] {
   if (Array.isArray(item)) {
     const len = item.length;
     const items = item.map((i) => encodeItem(i)).flat();
-    return [DataType.Array, len, ...items];
+    return [DataType.Array, ...encodeLength(len), ...items];
   } else if (typeof item === 'string') {
     return encodeString(item);
   } else if (Number.isInteger(item)) {
@@ -72,24 +73,29 @@ export function encodeItem(item: Item): number[] {
   throw new Error(`unknown item type: ${item}`)
 }
 
+function decodeLength(buffer: Uint8Array, offset: number): [len: number, offset: number] {
+  const lenOfLen = buffer[offset++];
+  const dataView = new DataView(buffer.buffer, offset, lenOfLen);
+  return [dataView.getInt32(0), offset + dataView.byteLength];
+}
+
 export function decodeItem(buffer: Uint8Array, offset: number = 0): [Item, number] {
   // let offset = 0;
   const dataType = buffer[offset++];
-  console.log("[parsing]", { dataType: DataType[dataType] })
-
+  const [len, nextOffset] = decodeLength(buffer, offset);
+  offset = nextOffset;
   if (dataType === DataType.Array) {
-    const itemCount = buffer[offset++];
+    const itemCount = len;
     const items: Item[] = [];
     for (let i = 0; i < itemCount; i++) {
-      console.log('child sliced:', buffer.slice(offset))
-      const [decoded, childLen] = decodeItem(buffer, offset);
+      const [decoded, end] = decodeItem(buffer, offset);
       items.push(decoded);
-      offset = childLen;
+      offset = end;
     }
     return [items, offset];
   }
 
-  const len = buffer[offset++];
+  // const len = buffer[offset++];
   if (dataType === DataType.String) {
     const encodedStr = buffer.slice(offset, offset + len);
     const str = decoder.decode(encodedStr);
@@ -116,11 +122,32 @@ Deno.test("encode int", () => {
   expect(decoded).toBe(1);
 });
 
+Deno.test("encode int negative", () => {
+  const input = -1;
+  const encoded = encode(input);
+  const decoded = decode(encoded);
+  expect(decoded).toBe(input);
+});
+
+
+Deno.test("encode int over 255", () => {
+  const input = 500;
+  const encoded = encode(input);
+  const decoded = decode(encoded);
+  expect(decoded).toBe(input);
+});
+
+Deno.test("encode int over 65565", () => {
+  const input = 80000;
+  const encoded = encode(input);
+  const decoded = decode(encoded);
+  expect(decoded).toBe(input);
+});
+
 Deno.test("encode float", () => {
   const input = 3.14;
   const encoded = encode(input);
   const decoded = decode(encoded);
-  // assert(decoded === 3.14);
   expect(decoded).toBe(3.14);
 });
 
@@ -131,6 +158,14 @@ Deno.test("encode string", () => {
   expect(decoded).toBe(input);
 });
 
+Deno.test("encode string over 256 length", () => {
+  const input = "Hello".repeat(100);
+  const encoded = encode(input);
+  const decoded = decode(encoded);
+  expect(decoded).toBe(input);
+});
+
+
 Deno.test("encode array 1", () => {
   const input = [4];
   const encoded = encode(input);
@@ -139,6 +174,29 @@ Deno.test("encode array 1", () => {
 });
 
 Deno.test("encode array 2", () => {
+  const input = [1, 2];
+  const encoded = encode(input);
+  const decoded = decode(encoded);
+  expect(decoded).toEqual(input);
+});
+
+Deno.test("encode array over 255", () => {
+  const input = new Array(1000).fill(1);
+  const encoded = encode(input);
+  const decoded = decode(encoded);
+  expect(decoded).toEqual(input);
+});
+
+
+Deno.test("encode array item", () => {
+  const input = [1, "hello", 3.14];
+  const encoded = encode(input);
+  const decoded = decode(encoded);
+  expect(decoded).toEqual(input);
+});
+
+
+Deno.test("encode nested array", () => {
   const input = [1, 2];
   const encoded = encode(input);
   const decoded = decode(encoded);
